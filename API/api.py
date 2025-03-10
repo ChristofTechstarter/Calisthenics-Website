6ed5bd0d33b7240ac2445f7bf975749e8f543156
Christof Gaßmann <christof.gassmann@tn.techstarter.de>
2025-03-10 16:01:51 +0100

    Added Flask API

---
+++ API/api.py
@@ -0,0 +1,126 @@
+from flask import Flask, request, jsonify
+import json
+
+app = Flask(__name__)
+
+
+def load_json(key):
+    with open("users_list.json", "r") as file_user:
+        content = file_user.read()
+        return json.loads(content)[key]
+
+
+def save_json_partial(key, index, updated_data, filename="users_list.json"):
+    with open(filename, "r+") as file:
+        file_data = json.load(file)
+        file_data[key][index] = updated_data
+        file.seek(0)
+        json.dump(file_data, file, indent=4)
+        file.truncate()  # Entfernt eventuelle alte Datenreste
+
+
+def add_json(key, new_data, filename="users_list.json"):
+    with open(filename, "r+") as file:
+        # First we load existing data into a dict.
+        file_data = json.load(file)
+        # Join new_data with file_data inside a given key
+        file_data[key].append(new_data)
+        # Sets file's current position at offset.
+        file.seek(0)
+        # convert back to json.
+        json.dump(file_data, file, indent=4)
+
+
+def delete_json(key, removevalue, filename="users_list.json"):
+    with open(filename, "r+") as file:
+        # First we load existing data into a dict.
+        file_data = json.load(file)
+        # Join new_data with file_data inside a given key
+        file_data[key].remove(removevalue)
+        # Sets file's current position at offset.
+        file.seek(0)
+        # convert back to json.
+        json.dump(file_data, file, indent=4)
+        file.truncate()
+
+
+# Users
+
+
+@app.route("/users")
+def get_users():
+    return jsonify(load_json("users"))
+
+
+@app.route("/users/signup", methods=["POST"])
+def signup():
+    users = load_json("users")
+    id = max([p["id"] for p in users], default=0) + 1
+    data = request.get_json()
+    email = data["email"]
+    username = data["username"]
+    password = data["password"]
+    first_name = data["firstName"]
+    family_name = data["familyName"]
+    for user in users:
+        if user["username"] == username:
+            return jsonify({"message": f"The Username {username} is already used!"})
+        elif user["email"] == email:
+            return jsonify({"message": f"The Email {email} is already used!"})
+    new_user = {
+        "id": id,
+        "email": email,
+        "username": username,
+        "password": password,
+        "firstName": first_name,
+        "familyName": family_name,
+    }
+    add_json("users", new_user)
+    return jsonify(
+        {"message": f"Successfully signed up a New User!", "Data saved": f"{new_user}"}
+    )
+
+
+@app.route("/users/update", methods=["PUT"])
+def update_username():
+    users = load_json("users")
+    data = request.get_json()
+    username = data["username"]
+    password = data["password"]
+    new_username = data["newUsername"]
+    for user in users:
+        if user["username"] == username:
+            if user["password"] == password:
+                user["username"] = new_username
+                save_json_partial("users", users.index(user), user)
+                return jsonify(
+                    {"message": f"Successfully changed {username} to {new_username}!"}
+                )
+            else:
+                return jsonify(
+                    {"message": f"Your Password for User {username} is incorrect!"}
+                )
+    return jsonify({"message": f"No Username: {username} was found!"})
+
+
+@app.route("/users/delete", methods=["DELETE"])
+def delete_user():
+    users = load_json("users")
+    data = request.get_json()
+    username = data["username"]
+    password = data["password"]
+    for user in users:
+        if user["username"] == username:
+            if user["password"] == password:
+                delete_json("users", user)
+
+                return jsonify({"message": f"Successfully deleted User!"})
+            else:
+                return jsonify(
+                    {"message": f"Your Password for User {username} is incorrect!"}
+                )
+    return jsonify({f"message": f"No User {username} found!"})
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=6060)
